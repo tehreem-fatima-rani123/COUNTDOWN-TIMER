785245db0d162a0c5ce65b0c113e68a0f355bc2f
TEHREEM FATIMA <168282752+tehreem-fatima-rani123@users.noreply.github.com>
2025-02-22 12:28:55 -0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,46 +1,46 @@
-#! /usr/bin/evn node
-
+#! /usr/bin/env node
 
 import inquirer from "inquirer";
+import { differenceInSeconds } from "date-fns";
 
-
-import {areIntervalsOverlapping, differenceInSeconds} from "date-fns"
-import { interval } from "date-fns/interval";
 const res = await inquirer.prompt([
     {
-name:"userInput",
-type:"number",
-message:"please enter the amount of second",
-validate:(input) =>{
-    if(isNaN(input)){
-        return"please emter vaild number"
-    }else if (input > 60){
-        return "second must be in 60"
-    }else{
-        return true;
+        name: "userInput",
+        type: "number",
+        message: "Please enter the amount of seconds (max 60):",
+        validate: (input) => {
+            if (isNaN(input)) {
+                return "Please enter a valid number";
+            } else if (input > 60 || input <= 0) {
+                return "Seconds must be between 1 and 60";
+            } else {
+                return true;
+            }
+        }
     }
-}
-}
 ]);
 
-let input = res.userInput
+let input: number = res.userInput;
 
-function startTime (val :number){
-    const intTime = new Date ().setSeconds(new Date ().getSeconds()+val);
+function startTime(val: number) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
     const IntervalTime = new Date(intTime);
-    setInterval((()=> {
-        const currTime = new Date()
-        const timeDiff = differenceInSeconds(IntervalTime,currTime);
 
-        if(timeDiff <= 0){
-            console.log("Timer has expired");
-            process.exit()
+    const timer = setInterval(() => {
+        const currTime = new Date();
+        const timeDiff = differenceInSeconds(IntervalTime, currTime);
+
+        if (timeDiff <= 0) {
+            console.log("⏳ Timer has expired!");
+            clearInterval(timer);
+            process.exit();
         }
-        const min = Math.floor((timeDiff%(3600*24))/3600)
-        const sec = Math.floor(timeDiff%60);
-        console.log(`${min.toString().padStart(2,"0")}:${sec.toString().padStart(2,"0")}`);
-        
-    }), 1000)
+
+        const min = Math.floor(timeDiff / 60);
+        const sec = timeDiff % 60;
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+
+    }, 1000);
 }
 
 startTime(input);
